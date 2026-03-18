4fbd00d4587c2a8f2edcc5769e800943810a49b3
labidi-houssem <houssemm.labidi@gmail.com>
2026-03-18 17:31:59 +0100

    crud by houssem

--- src/main/java/org/example/gestionuser/RestController/UserController.java
+++ src/main/java/org/example/gestionuser/RestController/UserController.java
@@ -2,13 +2,40 @@ package org.example.gestionuser.RestController;
 
 import lombok.AllArgsConstructor;
 import org.example.gestionuser.Services.IUser;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.example.gestionuser.entities.User;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @AllArgsConstructor
 @RequestMapping("/api/user")
 public class UserController {
     private final IUser iu;
+    @GetMapping("/getAll")
+    public List<User> getAllUser() {
+        return iu.getAllUsers();
+    }
+
+    @GetMapping("/getUser/{id}")
+    public User getUser(@PathVariable long id) {
+        return iu.getUser(id);
+    }
+
+    @PostMapping("/addUser")
+    public User addUser(@RequestBody User user){
+        return iu.adduser(user);
+    }
+
+    @PutMapping("/updateaUser")
+    public User update(@RequestBody User user){
+        return iu.updateUser(user);
+    }
+
+    @DeleteMapping("/del/{id}")
+    public void del(@PathVariable long id){
+        iu.removeUser(id);
+    }
+
 
 }
--- src/main/java/org/example/gestionuser/Services/UserServiceImp.java
+++ src/main/java/org/example/gestionuser/Services/UserServiceImp.java
@@ -1,4 +1,37 @@
 package org.example.gestionuser.Services;
 
+import lombok.AllArgsConstructor;
+import org.example.gestionuser.Repositories.UserRepo;
+import org.example.gestionuser.entities.User;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+@AllArgsConstructor
 public class UserServiceImp implements IUser{
+    private UserRepo ur;
+    @Override
+    public List<User> getAllUsers() {
+        return ur.findAll();
+    }
+
+    @Override
+    public User updateUser(User user) {
+        return ur.save(user);
+    }
+    @Override
+    public User adduser(User user) {
+        return ur.save(user);
+    }
+
+    @Override
+    public User getUser (long iduser) {
+        return ur.findById(iduser).get();
+    }
+    @Override
+    public void removeUser(long iduser) {
+        ur.deleteById(iduser);
+
+    }
+
 }
